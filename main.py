1dc95f5ef68a1cfd8832154dd1f42a0cd7395684
yukta1103 <96045555+yukta1103@users.noreply.github.com>
2024-11-28 22:18:14 -0500

    debug

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from poke_env import AccountConfiguration
 from poke_env import cross_evaluate
 import max_damage as md
 from gymnasium.utils.env_checker import check_env
-from rl_player import RLPlayer
+from rl_player import SimpleRLPlayer
 
 # The RandomPlayer is a basic agent that makes decisions randomly,
 # serving as a starting point for more complex agent development.
@@ -14,14 +14,14 @@ from rl_player import RLPlayer
 import asyncio
 
 async def main():
-    # my_account_config1 = AccountConfiguration("my_username_123", None)
+    my_account_config1 = AccountConfiguration("my_username_123", None)
     # my_account_config2 = AccountConfiguration("my_username_12345", None)
     
     random_player = RandomPlayer(battle_format="gen8randombattle")
-    rl_player = RLPlayer(battle_format="gen8randombattle", opponent=random_player)
+    rl_player = SimpleRLPlayer(battle_format="gen8randombattle", opponent=random_player)
 
 
     await random_player.battle_against(rl_player, n_battles=1)
 
 if __name__ == "__main__":
-        asyncio.get_event_loop().run_until_complete(main())
+    asyncio.get_event_loop().run_until_complete(main())
